458ffc87f06f0e9cd613ce91f74f3b6b5a973687
IlhamBudi16 <ilhambudimansyah13@ndata.my.id>
2025-03-08 12:47:05 +0700

    update

--- app/Controllers/Api/DosenApiController.php
+++ app/Controllers/Api/DosenApiController.php
@@ -43,31 +43,97 @@ class DosenApiController extends BaseController
     
     public function create()
     {
-        $data = [
-            "id_dosen" => $this->request->getPost("id_dosen"),
-            "nama_dosen" => $this->request->getPost("nama_dosen"),
-            "email" => $this->request->getPost("email"),
-            "no_telp" => $this->request->getPost("no_telp")
-        ];
-        $this->m_dosen->insert($data);
+        // fungsi untuk menyimpan data ke tabel dosen
+        try {
+            $rules = [
+                'nama_dosen' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'nama dosen harus di isi!'
+                    ]
+                ],
+                'email' => [
+                    'rules' => 'required|valid_email',
+                    'errors' => [
+                        'required' => 'email harus di isi!',
+                        'valid_email' => 'email tidak valid'
+                    ]
+                ],
+                'no_telp' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'no telp harus di isi!'
+                    ]
+                ]
+            ];
+            
+            if (!$this->validate($rules)) {
+                return $this->fail($this->validator->getErrors());
+            }
+    
+            $data = [
+                "id_dosen" => $this->request->getPost("id_dosen"),
+                "nama_dosen" => $this->request->getPost("nama_dosen"),
+                "email" => $this->request->getPost("email"),
+                "no_telp" => $this->request->getPost("no_telp")
+            ];
+            $this->m_dosen->insert($data);
+    
+            $res = [
+                "status" => 201,
+                "message" => "Data dosen berhasil dibuat!",
+                "data" => $data
+            ];
+            return $this->respond($res);
+            
+        } catch (\Exception $e) {
+            return $this->failServerError($e->getMessage());
+        }
+    }    
 
-        $res = [
-            "status" => 201,
-            "message" => "Data dosen berhasil dibuat!",
-            "data" => $data
-        ];
-        return $this->respond($res);
-    }
-
-    // Update mahasiswa by id_dosen
+    // Update dosen by id_dosen
     public function update($id_dosen)
     {
-        $data = $this->request->getRawInput();
+        try {
+            $rules = [
+                'nama_dosen' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'Nama dosen harus diisi!'
+                    ]
+                ],
+                'email' => [
+                    'rules' => 'required|valid_email',
+                    'errors' => [
+                        'required' => 'Email harus diisi!',
+                        'valid_email' => 'Email tidak valid'
+                    ]
+                ],
+                'no_telp' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'No telepon harus diisi!'
+                    ]
+                ]
+            ];
 
-        if ($this->m_dosen->update($id_dosen, $data)) {
-            return $this->respond(['message' => 'Data dosen berhasil diupdate.']);
-        } else {
-            return $this->failValidationErrors($this->m_dosen->errors());
+            if (!$this->validate($rules)) {
+                return $this->fail($this->validator->getErrors());
+            }
+
+            $data = [
+                'nama_dosen' => $this->request->getRawInput()['nama_dosen'] ?? null,
+                'email' => $this->request->getRawInput()['email'] ?? null,
+                'no_telp' => $this->request->getRawInput()['no_telp'] ?? null
+            ];
+
+            if ($this->m_dosen->update($id_dosen, $data)) {
+                return $this->respond(['message' => 'Data dosen berhasil diupdate.']);
+            } else {
+                return $this->failValidationErrors($this->m_dosen->errors());
+            }
+        } catch (\Exception $e) {
+            return $this->failServerError($e->getMessage());
         }
     }
 
